3cce9adbfbd3bc33f454338ecdac3c800efc6338
Moureen Islam <106474784+Moureen-Islam@users.noreply.github.com>
2022-10-03 18:19:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,3 @@
-
-// function liferemaining(age){
-//     var years = 70-age;
-//     var days = years * 365;
-//     var weeks = years *52;
-//     var months = years *12;
-//     console.log ("You have "+days+"days"+weeks+"weeks"+months+"months"+years+"years remaining");
-
-// }
-// liferemaining(28);
-
-// let button = document.querySelector('#btn');
-
-// button.addEventListener('click', calculateBMI); 
-// function calculateBMI(){
-//     let height = document.querySelector('#height').value;
-//     let weight = document.querySelector('#weight').value;
-//     let result = (weight / ((height * height)/ 10000)).toFixed(2);
-//     document.querySelector('#result').value = result;
-
-// }
-    
 let button = document.querySelector('#btn');
 button.addEventListener('click', calculateBMI);
 function calculateBMI () {
@@ -45,18 +23,17 @@ function calculateBMI () {
     else if ( result > 25){
         document.querySelector(".text").innerHTML =" You are Overweight!"
 
-    } else if (isNaN = result){
+    } else if (isNaN(result)){
         document.querySelector(".text").innerHTML ="Please provide the necessary information!"
     }
     
     else{
-        document.querySelector(".text").innerHTML =" You are Healthy!"
+        document.querySelector(".text").innerHTML ="You are Healthy!"
     }
 
     
 
 }
-
 
 
 
